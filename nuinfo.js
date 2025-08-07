cea357bdf77121fd62298955afef162907654f37
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-08-07 16:17:23 +0300

    Update nuinfo.js

--- nuinfo.js
+++ nuinfo.js
@@ -128,17 +128,15 @@ autoFillBtn.addEventListener('click', () => {
   const altTitle = nuInfoResults.querySelector('#editassociated')?.textContent || '';
   const date = nuInfoResults.querySelector('#edityear')?.textContent || '';
   const statuscoo = nuInfoResults.querySelector('#editstatus')?.textContent || '';
-  const language = nuInfoResults.querySelector('#editlanguage')?.textContent || '';
-  const originalPublisher = nuInfoResults.querySelector('#editoriginalPublisher')?.textContent || '';
-
+  const language = nuInfoResults.querySelector('#showlang a')?.textContent || '';
+  const originalPublisher = nuInfoResults.querySelector('#showopublisher a')?.textContent || '';
 
   const editCover = document.getElementById('editCover');
   const editAltTitle = document.getElementById('editaltitile');
   const editDate = document.getElementById('editdate');
-  const editstatuscoo = document.getElementById('editstatus');
+  const editstatuscoo = document.getElementById('editstatuscoo');
   const editlanguage = document.getElementById('editlanguage');
   const editoriginalPublisher = document.getElementById('editoriginalPublisher');
-
 
   if (editCover) editCover.value = coverUrl;
   if (editAltTitle) editAltTitle.value = altTitle;
